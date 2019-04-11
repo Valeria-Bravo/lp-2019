9e549bb41b743fb1d48080e2fbc670448128bd28
Valeria-Bravo <49079491+Valeria-Bravo@users.noreply.github.com>
2019-04-11 16:58:55 -0500

    Add files via upload

---
+++ Basic/orden.cpp
@@ -0,0 +1,32 @@
+include<iostream>
+
+using namespace std;
+int main(){
+	cout<<"Ingrese los numeros seguidos de espacio"<<endl;
+	int x,y,z;
+  cin>>z>>y>>x;
+  
+	int mayor, medio, menor;
+	mayor=z;
+	medio=y;
+	menor=x;
+	
+	
+		if(x>y){
+			menor=y;
+			medio=x;
+		}
+		if(x>z){
+			mayor=x;
+			menor=z;
+		}
+		if(y>z){
+			mayor=y;
+			medio=z;
+		}
+	
+		cout<<"mayor es "<<x<<endl;
+		cout<<"medio es "<<y<<endl;
+		cout<<"menor es "<<z<<endl;
+	
+}
