615851ef9c34924d41d02f6f08b6c44a37721257
Valeria-Bravo <49079491+Valeria-Bravo@users.noreply.github.com>
2019-05-03 11:37:40 -0500

    Delete cod_04.cpp

--- Basic2/cod_04.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std; 
-int area (int length , int width ) ;
-int main ( )
-{
-  int x = area (2,3) ;
-}
-double  area (double x , double y) {return x*y ; }
-int ara (int x,int y,char unit) { return x*y ; }
