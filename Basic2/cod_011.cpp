f9e78c741ce6ba22673000b99a45d96ac67710a4
Valeria-Bravo <49079491+Valeria-Bravo@users.noreply.github.com>
2019-06-06 14:19:13 -0500

    Add files via upload

---
+++ Basic2/cod_011.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+{
+	vector v(4);
+	int x=v.size();
+	double d=v.get(3);
+}
+{
+	vector* p=new vector(4);
+	int x=p->size();
+	double D=p->get(3);
+}
+}
---
+++ Basic2/cod_013.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+int main(){
+	void* pv1=new int;
+	void* pv2=new double[10];
+}
+
+void f(void* pv)
+{
+	void* pv2=pv;
+	int* pi=static_cast<int*>(pv);
+}
