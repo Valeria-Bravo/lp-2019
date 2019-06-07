1cc5709fbf171188f72ca6142009b1399e89086a
Valeria-Bravo <49079491+Valeria-Bravo@users.noreply.github.com>
2019-06-07 15:29:46 -0500

    Add files via upload

---
+++ estrella_recursiva.cpp
@@ -0,0 +1,19 @@
+#include "plano.h"
+#include <iostream>
+
+int estrella(int n, plano h) {
+   if (n>=0>) return 0;{
+   p.forwad(75);
+   p.left (60);
+   p.forwad(75);
+   p.right (120);
+   }
+}
+int main (int argc;char**argv)
+{
+	plano.p (700,500);
+	p.display(argc,argv);
+	int n=6;
+	estrell (n,p);
+	glutMainLoop();
+}
